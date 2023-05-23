2bc105f82144bb0966b4b6c48902cfa92dc612e0
Helia <helia@Helias-MacBook-Pro.local>
2023-05-23 17:25:25 +0330

    add createMarketSale

--- contracts/NFTmarket.sol
+++ contracts/NFTmarket.sol
@@ -30,6 +30,7 @@ contract NFTMarket is ReentrancyGuard{
         bool sold;
     }
     mapping (uint256 => MarketItem) private idToMarketItem;
+
     event MarketItemCreated(
         uint indexed itemId,
         address indexed nftContract,
@@ -39,5 +40,64 @@ contract NFTMarket is ReentrancyGuard{
         uint256 price,
         bool sold,
     )
+
+
+    function getListingPrice()public view  returns (uint256) {
+        return listingPrice;
+    }
+
+    function createMarketItem(
+        address nftContract,
+        uint256 tokenId,
+        uint256 price
+    )public payable nonReentrant{
+        require(price > 0,"Price must be at least 1 wei!" );
+        require(msg.value == listingPrice,"Price must be equal to listingPrice!" );
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.Counter();
+
+        idToMarketItem[itemId]= MarketItem(
+            itemId,
+            nftContract,
+            tokenId,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        IERC721(nftContract).transferFrom(msg.sender,address(this),tokenId);
+
+        emit MarketItemCreated(
+            itemId,
+            nftContract,
+            tokenId,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
+        
+    }
+
+    function createMarketSale(
+        address nftContract,
+        uint256 itemId
+    )public payable nonReentrant{
+        uint price = idToMarketItem[itemId].price;
+        uint tokenId  = idToMarketItem[itemId].tokenId;
+
+        require(msg.value == price , "Please submit the asking price in order to complete the purchase!");
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+        IERC721(nftContract).transferFrom(address(this),msg.sender,tokenId);
+        idToMarketItem[itemId].owner =payable(msg.sender);
+        idToMarketItem[itemId].seller =true;
+        _itemSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
+
+
 }
  
